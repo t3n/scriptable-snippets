6b9ad1c17c2107ad47cd239081a5a0c4150ded60
Philip Plagge <plaggep@gmail.com>
2020-11-06 10:48:41 +0100

    Improve widget for different sizes

--- t3n-news.js
+++ t3n-news.js
@@ -14,13 +14,13 @@ async function fetchData() {
     "Content-Type": "application/json",
   };
   req.body = JSON.stringify({
-    operationName: null,
+    operationName: "ScriptableLatestNews",
     variables: {},
     query:
-      "{\n  article {\n    recentNews(limit: 1) {\n      identifier\n      title\n      url\n      imageUrl(width: 500, height: 500)\n    }\n  }\n}\n",
+      "query ScriptableLatestNews {\n  article {\n    recentNews(limit: 4) {\n      identifier\n      title\n      url\n      imageUrl(width: 500, height: 500)\n    }\n  }\n}\n",
   });
   const { data } = await req.loadJSON();
-  const news = data.article.recentNews[0];
+  const news = data.article.recentNews;
 
   return news;
 }
@@ -37,32 +37,53 @@ async function createWidget() {
   widget.backgroundColor = new Color("#f9423a");
 
   try {
-    const data = await fetchData();
-    const { title, imageUrl, url } = data;
-
-    const image = await loadImage(imageUrl);
-    widget.backgroundImage = image;
-
-    const gradient = new LinearGradient();
-    gradient.locations = [0, 1];
-    gradient.colors = [new Color("#f9423a", 0.8), new Color("#f9423a", 1)];
-    widget.backgroundGradient = gradient;
-
     const logoImage = await loadImage(
       "https://d1quwwdmdfumn6.cloudfront.net/t3n/2018/images/logos/t3n_logo_420x420.png"
     );
+    const logoSize = config.widgetFamily === "large" ? 60 : 40;
     const logo = widget.addImage(logoImage);
-    logo.imageSize = new Size(40, 40);
+    logo.imageSize = new Size(logoSize, logoSize);
+
+    const data = await fetchData();
+    const images = await Promise.all(
+      data.map((item) => loadImage(item.imageUrl))
+    );
+
+    data
+      .slice(0, config.widgetFamily === "large" ? undefined : 1)
+      .map(async (item, i) => {
+        widget.addSpacer();
+
+        const { title, url } = item;
+
+        if (i === 0) {
+          const image = images[i];
+          widget.backgroundImage = image;
+
+          const gradient = new LinearGradient();
+          gradient.locations = [0, 1];
+          gradient.colors = [
+            new Color("#f9423a", 0.8),
+            new Color("#f9423a", 1),
+          ];
+          widget.backgroundGradient = gradient;
+
+          if (config.widgetFamily !== "large") widget.url = url;
+        }
+
+        const itemText = widget.addText(title);
+
+        itemText.font = Font.boldSystemFont(
+          config.widgetFamily !== "small" && i === 0 ? 18 : 14
+        );
+        itemText.textColor = Color.white();
+
+        if (config.widgetFamily === "large") itemText.url = url;
+      });
+
+    if (config.widgetFamily === "large") widget.url = "https://t3n.de";
 
     widget.addSpacer();
-
-    const titleText = widget.addText(title);
-    titleText.font = Font.boldSystemFont(14);
-    titleText.textColor = Color.white();
-
-    widget.addSpacer();
-
-    widget.url = url;
 
     return widget;
   } catch (err) {
